af02816af4381e6ecfeb4e37f4208a2f2a35f2bc
anbu-selvan10 <anbuselvan253@gmail.com>
2025-02-28 21:24:50 +0530

    changing stock analyst to personalized stock analyst #1

--- backend/agents/stock_analyst.py
+++ backend/agents/stock_analyst.py
@@ -3,12 +3,18 @@ import yfinance as yf
 from phi.model.azure import AzureOpenAIChat
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.postgres import PostgresTools
 from dotenv import load_dotenv
 import phi
 import os
 
 load_dotenv(r"..\.env")
 
+SUPABASE_USER = os.getenv('SUPABASE_USER')
+SUPABASE_PASSWORD = os.getenv('SUPABASE_PASSWORD')
+SUPABASE_HOST = os.getenv('SUPABASE_HOST')
+SUPABASE_DB = os.getenv('SUPABASE_DB')
+SUPABASE_PORT = os.getenv('SUPABASE_PORT')
 PHI_KEY = os.getenv("PHI_KEY")
 AZURE_KEY = os.getenv("AZURE_KEY")
 AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
@@ -21,6 +27,14 @@ azure_model = AzureOpenAIChat(
     api_key=AZURE_KEY,
     azure_endpoint=AZURE_ENDPOINT,
     azure_deployment=AZURE_DEPLOYMENT,
+)
+
+postgres_tools = PostgresTools(
+    host=SUPABASE_HOST,
+    port=int(SUPABASE_PORT),
+    db_name=SUPABASE_DB,
+    user=SUPABASE_USER, 
+    password=SUPABASE_PASSWORD
 )
 
 search_agent = Agent(
@@ -40,6 +54,18 @@ stock_agent = Agent(
     show_tool_calls=True,
     description="An investment analyst researching Indian stock prices, analyst recommendations, and stock fundamentals.",
     instructions=["Provide stock data in a concise manner to show to the user"],
+    markdown=True
+)
+
+personalized_agent = Agent(
+    name="Personal Agent",
+    model=azure_model,
+    tools=[postgres_tools],
+    instructions=[
+        "Retrieve the user's saved amount for each of the previous months.",
+        "This can be done by summing budget amount and summing expense amount regardless of categories and subtracting.",
+        "Provide small and concise response",
+    ],
     markdown=True
 )
 
